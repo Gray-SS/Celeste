e6e5e291dfeb46f3099e8a7828c96327389ebe5b
Sasha Klinkemallie <76062492+Gray-SS@users.noreply.github.com>
2023-08-12 00:47:52 +0200

    feat: added object visualizers, they're used to render object into the properties window

---
+++ Editor/Celeste.Editor/Rendering/IObjectVisualizer.cs
@@ -0,0 +1,10 @@
+﻿namespace Celeste.Editor.Rendering;
+
+public interface IObjectVisualizer
+{
+    Type RenderType { get; }
+
+    EditorRuntime Editor { get; }
+
+    void Render(object target);
+}
---
+++ Editor/Celeste.Editor/Rendering/ObjectVisualizer.cs
@@ -0,0 +1,19 @@
+﻿namespace Celeste.Editor.Rendering;
+
+public abstract class ObjectVisualizer<T>
+    : IObjectVisualizer
+{
+    public Type RenderType => typeof(T);
+
+    public EditorRuntime Editor { get; }
+
+    public ObjectVisualizer(EditorRuntime editor)
+    {
+        Editor = editor;
+    }
+
+    protected abstract void OnImGuiRender(T target);
+
+    void IObjectVisualizer.Render(object target)
+        => OnImGuiRender((T)target);
+}
